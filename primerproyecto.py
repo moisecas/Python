795c047ebcbaeb4f858e717970e886e29549e6cd
unknown <moisesjuniorcas20@gmail.com>
2023-06-18 06:27:17 -0500

    input

--- primerproyecto.py
+++ primerproyecto.py
@@ -6,3 +6,10 @@ print("Estudiar con \"Python Total\" es super divertido")
 #tipo de dato string
 print(type("hola mundo")) #str 
 print("una linea \n dos lineas \n otra linea") #salto de linea \n
+print("una linea \t dos lineas \t otra linea") #tabulacion \t
+
+print("Barra Normal: /")
+print("Barra Invertida: \\")
+
+#input
+input("Cual es tu nombre? ")
