88e6d65353cce33eab05588930ae71d86341ad5c
unknown <moisesjuniorcas20@gmail.com>
2023-06-18 05:30:59 -0500

    first commit

---
+++ primerproyecto.py
@@ -0,0 +1,8 @@
+print("hola mundo")
+
+# print Estudiar con "Python Total" es super divertido
+print("Estudiar con \"Python Total\" es super divertido")
+
+#tipo de dato string
+print(type("hola mundo")) #str 
+print("una linea \n dos lineas \n otra linea") #salto de linea \n
